d1506a949f42bf649375cf6f78a5ea67fdbb6cef
Muhammad Tahir <muhammadtahirbharchoond@gmail.com>
2024-06-22 12:12:48 +0500

    home page and container page
    created home page and container page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,22 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      
+      <div className="relative">
+        <div className="absolute"></div>
+      </div>
+
+      <div className="">
+        <div className="overflow-hidden">
+          <div className="relative h-screen">
+            <img
+            className="absolute top-0 transition ease-in duration-200 max-xl:right-[-1050px] xl:right-[-970px] image"
+              width="4377"
+              src="https://github.githubassets.com/images/modules/site/home-campaign/hero-bg.webp"
+              alt="bg-image"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
